e9185defb7c325644d52cb944516b9f84b1ec4aa
ngdanghau <ngdghau201@gmail.com>
2022-03-25 09:56:04 +0700

    thêm event và control StatisticActivity ở HomeActivity

--- app/src/main/java/com/example/stdmanager/HomeActivity.java
+++ app/src/main/java/com/example/stdmanager/HomeActivity.java
@@ -2,13 +2,34 @@ package com.example.stdmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
 
+    ImageButton btnStatistic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        setControl();
+        setEvent();
+    }
+
+    private void setControl(){
+        btnStatistic = findViewById(R.id.btnStatistic);
+    }
+
+    private void setEvent(){
+        btnStatistic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, StatisticActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
