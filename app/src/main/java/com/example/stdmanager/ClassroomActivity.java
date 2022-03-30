86be0741e7455a34090a657a091ff3531249a46b
Phong Kaster <phongkaster@gmail.com>
2022-03-30 21:32:31 +0700

    30-03-2022 tạo Student & Grade OpenHelper

--- app/src/main/java/com/example/stdmanager/ClassroomActivity.java
+++ app/src/main/java/com/example/stdmanager/ClassroomActivity.java
@@ -3,12 +3,99 @@ package com.example.stdmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.ListView;
+
+import com.example.stdmanager.DB.GradeOpenHelper;
+import com.example.stdmanager.DB.StudentOpenHelper;
+import com.example.stdmanager.listViewModels.ClassroomListViewModel;
+import com.example.stdmanager.models.Grade;
+import com.example.stdmanager.models.Student;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ArrayList;
 
 public class ClassroomActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<Student> objects = new ArrayList<>();
+    ClassroomListViewModel listViewModel;
+
+    ArrayList<Grade> gradeObjects;
+    GradeOpenHelper gradeOpenHelper = new GradeOpenHelper(this);
+    StudentOpenHelper studentOpenHelper = new StudentOpenHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classroom);
+
+        /*The command line belows that make sure that keyboard only pops up only if user clicks into EditText */
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+
+
+//        gradeOpenHelper.deleteAndCreatTable();
+//        gradeObjects = gradeOpenHelper.retrieveAllGrades();
+//
+//        for( int i = 0 ; i < gradeObjects.size(); i++)
+//        {
+//            Log.d("Dtext", gradeObjects.get(i).getName());
+//        }
+
+        studentOpenHelper.deleteAndCreateTable();
+        objects = studentOpenHelper.retrieveAllStudents();
+
+        setControl();
+        Log.d("text","Hello");
+        for( int i = 0 ; i < objects.size() ; i++)
+        {
+            Student student = objects.get(i);
+            String message = student.getId() + " " +
+                    student.getFamilyName() + " " +
+                    student.getFirstName() + " " +
+                    student.getGender() + " " +
+                    student.getBirthday() + " " +
+                    student.getGradeId();
+            Log.d("text", message );
+        }
+
+        setEvent();
+    }
+
+    private void setControl()
+    {
+        listView = findViewById(R.id.classroomListView);
+    }
+
+    /**
+     * this function is used to create default records in order to check
+     * classroomListViewModel works properly or not ?
+     * */
+    private void initiativeDataExample()
+    {
+        /*Step 1*/
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Date today = new Date();
+        String birthday = simpleDateFormat.format(today);
+
+        /*Step 2*/
+        Student student1 = new Student("Nguyen Thanh", "Phong", birthday);
+        Student student2 = new Student("Nguyen Van", "Chung" , birthday);
+        Student student3 = new Student("Nguyen Dang", "Hau", birthday);
+
+        /*Step 3*/
+        objects.add(student1);
+        objects.add(student2);
+        objects.add(student3);
+    }
+
+    private void setEvent()
+    {
+        //initiativeDataExample();
+        listViewModel = new ClassroomListViewModel(this, R.layout.activity_classroom_element, objects);
+        listView.setAdapter(listViewModel);
     }
 }
---
+++ app/src/main/java/com/example/stdmanager/DB/StudentOpenHelper.java
@@ -0,0 +1,217 @@
+package com.example.stdmanager.DB;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.example.stdmanager.models.Grade;
+import com.example.stdmanager.models.Student;
+
+import java.util.ArrayList;
+
+public class StudentOpenHelper extends SQLiteOpenHelper {
+
+    private static final String TABLE_NAME = "student";
+    private static final  String REFERENCED_TABLE_NAME = "grade";
+    private static final String TAG = "SQLite";
+
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_FAMILY_NAME = "familyName";
+
+    private static final String COLUMN_FIRST_NAME = "firstName";
+    private static final String COLUMN_GENDER = "gender";
+
+    private static final String COLUMN_BIRTHDAY = "birthday";
+    private static final String COLUMN_GRADE_ID = "gradeId";
+
+    public StudentOpenHelper(@Nullable Context context) {
+        super(context, DBConfig.getDatabaseName(), null, DBConfig.getDatabaseVersion());
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String query = String.format("CREATE TABLE %s" +
+                "( %s INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "%s TEXT, " +
+                "%s TEXT," +
+                "%s INTEGER," +
+                "%s TEXT, " +
+                "%s INTEGER REFERENCES [%s](id) ON DELETE CASCADE NOT NULL )",
+                TABLE_NAME, COLUMN_ID, COLUMN_FAMILY_NAME, COLUMN_FIRST_NAME, COLUMN_GENDER, COLUMN_BIRTHDAY, COLUMN_GRADE_ID, REFERENCED_TABLE_NAME);
+
+        sqLiteDatabase.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    /**
+     * @author Phong-Kaster
+     * @param student | object | the grade which is added to TABLE_GRADE
+     */
+    public void create(Student student)
+    {
+        /*Step 1*/
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        /*Step 2*/
+        String familyName = student.getFamilyName();
+        String firstName = student.getFirstName();
+
+        int gender = student.getGender();
+        String birthday = student.getBirthday();
+        int gradeId = student.getGradeId();
+
+        /*Step 3*/
+        contentValues.put(COLUMN_FAMILY_NAME, familyName);
+        contentValues.put(COLUMN_FIRST_NAME, firstName);
+        contentValues.put(COLUMN_GENDER, gender);
+        contentValues.put(COLUMN_BIRTHDAY, birthday);
+        contentValues.put(COLUMN_GRADE_ID, gradeId);
+
+
+        /*Step 4*/
+        sqLiteDatabase.insert(TABLE_NAME,null, contentValues);
+        sqLiteDatabase.close();
+    }
+
+    /**
+     * @author Phong-Kaster
+     * @param student | object | the grade which is deleted from TABLE_GRADE
+     */
+    public void delete(Student student)
+    {
+        /*Step 1*/
+        int id = student.getId();
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+
+        /*Step 2*/
+        sqLiteDatabase.delete(TABLE_NAME, COLUMN_ID + " = ?",
+                new String[]{ String.valueOf(id)} );
+        sqLiteDatabase.close();
+    }
+
+    public void update(Student student)
+    {
+        /*Step 1*/
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+
+        /*Step 2*/
+        int id = student.getId();
+        String familyName = student.getFamilyName();
+        String firstName = student.getFirstName();
+
+        int gender = student.getGender();
+        String birthday = student.getBirthday();
+        int gradeId = student.getGradeId();
+
+
+        /*Step 3*/
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_FAMILY_NAME, familyName);
+        values.put(COLUMN_FIRST_NAME, firstName);
+        values.put(COLUMN_GENDER, gender);
+        values.put(COLUMN_BIRTHDAY, birthday);
+        values.put(COLUMN_GRADE_ID, gradeId);
+
+
+        /*Step 4*/
+        sqLiteDatabase.update(TABLE_NAME, values, COLUMN_ID + " = ?",
+                new String[]{ String.valueOf(id) } );
+    }
+
+    public ArrayList<Student> retrieveAllStudents()
+    {
+        /*Step 1*/
+        ArrayList<Student> objects = new ArrayList<>();
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+
+
+
+        /*Step 2*/
+        String query = "SELECT s.*, g.name FROM student s INNER JOIN grade g ON s.gradeId = g.id";
+        @SuppressLint("Recycle") Cursor cursor = sqLiteDatabase.rawQuery(query, null);
+
+        /*Step 3*/
+        if( cursor.moveToFirst() )
+        {
+            do
+            {
+                Student student = new Student();
+
+                student.setId( Integer.parseInt( cursor.getString(0) ) );
+
+                student.setFamilyName( cursor.getString(1));
+                student.setFirstName(  cursor.getString(2));
+
+                student.setGender(Integer.parseInt( cursor.getString(3) ));
+                student.setBirthday(cursor.getString(4));
+
+                student.setGradeId(Integer.parseInt( cursor.getString(5) ));
+                student.setGradeName(cursor.getString(6));
+
+                objects.add(student);
+            }while( cursor.moveToNext() );
+        }
+
+        return objects;
+    }
+
+
+    /**
+     * @author Phong-Kaster
+     * retrieve number of grade
+     * @return int quantity
+     */
+    private int count()
+    {
+        String query = "SELECT * FROM " + TABLE_NAME;
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+
+        Cursor cursor = sqLiteDatabase.rawQuery(query, null);
+
+        int quantity = cursor.getCount();
+        cursor.close();
+        sqLiteDatabase.close();
+        return quantity;
+    }
+
+    /**
+     * @author Phong-Kaster
+     * create default records if there is nothing in Grade table
+     */
+    private void createDefaultRecords()
+    {
+        /*Step 1*/
+        int studentQuantity = this.count();
+        if( studentQuantity != 0)
+            return;
+
+        /*Step 2*/
+        Student student1 = new Student("Nguyen","Phong",0,"01-05-2000", 1);
+        Student student2 = new Student("Nguyen","Hau",0,"01-05-2000", 1);
+        Student student3 = new Student("Nguyen","Khang",0,"01-05-2000", 1);
+        Student student4 = new Student("Nguyen","Long",0,"01-05-2000", 100);
+        this.create(student1);
+        this.create(student2);
+        this.create(student3);
+       // this.create(student4);
+    }
+
+    public void deleteAndCreateTable()
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+        createDefaultRecords();
+    }
+}
